3dcd9501d93c3f8ddec433e99f3d78d5674070e0
Denis Borisov <borisovdenis-kb@yandex.ru>
2017-09-07 12:58:29 +0400

    Добавил ендпоинты для регистрации, логина и логаута пользователей.

--- test_task/urls.py
+++ test_task/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from test_task_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', views.login),
+    url(r'^logout/$', views.logout),
+    url(r'^registration/$', views.registration),
 ]
--- test_task_app/views.py
+++ test_task_app/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import auth
+from django.contrib.auth.models import Group
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def registration(request):
+    if request.method == 'POST':
+        data = request.POST
+        form = UserCreationForm(data)
+
+        if form.is_valid():
+            # создаем и добавляем пользователя в нужную группу
+            try:
+                group = Group.objects.get(name=data['groupname'])
+            except ObjectDoesNotExist:
+                return HttpResponse(status=400)
+            else:
+                new_user = form.save()
+                group.user_set.add(new_user)
+
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=400)
+    else:
+        return HttpResponse(status=405)
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponse(status=200)
+            else:
+                return HttpResponse(status=403)
+        else:
+            return HttpResponse(status=400)
+    else:
+        return HttpResponse(status=405)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponse(status=200)
